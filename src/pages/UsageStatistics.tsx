027ae652e6bb268f3ce82562c3a4484d29159dcd
hrushi4 <hrushikeshchavan257@gmail.com>
2023-11-11 21:27:16 +0530

    category_distribution added to the file

--- src/pages/UsageStatistics.tsx
+++ src/pages/UsageStatistics.tsx
@@ -22,20 +22,9 @@ const UsageStatistics = () => {
       <AdminSidebar />
       <main className="chart-container">
         <h1>Bar Charts</h1>
+
         <section>
           <BarChart
-            data_1={[200, 444, 343, 556, 778, 455, 990]}
-            data_2={[300, 144, 433, 655, 237, 755, 190]}
-            title_1="Products"
-            title_2="Users"
-            bgColor_1={`hsl(260,50%,30%)`}
-            bgColor_2={`hsl(360,90%,90%)`}
-          />
-          <h2>Top Selling Products & Top Customers</h2>
-        </section>
-        <section>
-          <BarChart
-            horizontal={true}
             data_1={[
               200, 444, 343, 556, 778, 455, 990, 444, 122, 334, 890, 909,
             ]}
