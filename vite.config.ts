fcf6a7f3f684417f68357c78ebb356d4e76816f5
hrushi4 <hrushikeshchavan257@gmail.com>
2023-11-13 14:36:42 +0530

    deployement configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000, // Specify the port you want to use
+    host: "0.0.0.0", // Allow connections from any IP address
+  },
+});
